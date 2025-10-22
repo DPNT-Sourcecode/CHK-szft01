86080712c5ef2d33befc68f3bad918d3b6a8d3ee
sbx_user1051 <sbx_user1051@169.254.39.17>
2025-10-22 15:06:49 +0000

    Wed Oct 22 15:06:49 UTC 2025

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -34,7 +34,8 @@ Our price table and offers:
 """
 import re
 
-# OK at the risk of taking loads of time I'm going for a big refactor
+# OK at the risk of taking loads of time I'm going for a big refactor - worked quite well!
+# still could do more but gonna plough on for now
 BASE_PRICES = {
     'A': 50,
     'B': 30,
@@ -135,13 +136,24 @@ PRODUCT_OFFERS = {
     'F': calculate_f_total,
     'G': lambda c: c * BASE_PRICES['G'],
     'H': calculate_h_total,
-    # 'K': [(2, 150)],
-    # 'N': [(3, 'get one M free')],
-    # 'P': [(5, 200)],
-    # 'Q': [(3, 80)],
-    # 'R': [(3, 'get one Q free')],
-    # 'U': [(3, 'get one U free')],
-    # 'V': [(2, 90), (3, 130)],
+    'I': lambda c: c * BASE_PRICES['I'],
+    'J': lambda c: c * BASE_PRICES['J'],
+    'K': calculate_k_total,
+    'L': lambda c: c * BASE_PRICES['L'],
+    'M': lambda c: c * BASE_PRICES['M'],
+    'N': calculate_n_total,
+    'O': lambda c: c * BASE_PRICES['O'],
+    'P': calculate_p_total,
+    'Q': calculate_q_total,
+    'R': calculate_r_total,
+    'S': lambda c: c * BASE_PRICES['S'],
+    'T': lambda c: c * BASE_PRICES['T'],
+    'U': calculate_u_total,
+    'V': calculate_v_total,
+    'W': lambda c: c * BASE_PRICES['W'],
+    'X': lambda c: c * BASE_PRICES['X'],
+    'Y': lambda c: c * BASE_PRICES['Y'],
+    'Z': lambda c: c * BASE_PRICES['Z'],
 }
 
 class CheckoutSolution:
@@ -164,12 +176,12 @@ class CheckoutSolution:
             else:
                 letter_counts[ltr] = 1
 
-        # for letter in BASE_PRICES.keys():
-        #     letter_counts[letter] = skus.count(letter)
-
         final_total = 0
 
-
+        # TODO explain
+        # TODO extract?
+        # if there are any cross product free items we have to do those first
+        # eg if there are Es we have to calculate those first to get free Bs
         for cross_product_ltr in FREE_OTHER_PROUDCTS.keys():
             if cross_product_ltr in letter_counts:
                 free_ltr = FREE_OTHER_PROUDCTS[cross_product_ltr]
@@ -182,60 +194,10 @@ class CheckoutSolution:
                     letter_counts.pop(free_ltr)
 
         for letter in letter_counts:
-            print(f"DAVE {letter} {letter_counts[letter]}")
             temp = PRODUCT_OFFERS[letter](letter_counts[letter])
-            print(f"DAVE TEMP {temp}")
             final_total += temp
 
         return final_total
-        
 
-        # cant think of a clean way to avoid these next 26 calculations
-        # could assign fucntions to each letter in BASE_PRICES and rename but there are dependencies like free Bs for E aso have to do E before B
-        # just gonna get it working for now
-        # just realsed the test coverage may expect different test files, really hope thats not the case
-
-        # goign to pause to see if our tests are actually evanluated because there is a LOT needed for CHK_4
-        # in a min
-
-        # a_total = self.calculate_a_total(letter_counts['A'])
-        # c_total = letter_counts['C'] * BASE_PRICES['C']
-        # d_total = letter_counts['D'] * BASE_PRICES['D']
-
-        # # order swap, free B with E
-        # e_total, b_free_count = self.calculate_e_total_and_b_free_count(letter_counts['E'])
-        # b_total = self.calculate_b_total(letter_counts['B'], b_free_count)
-
-        # f_total = self.calculate_f_total(letter_counts['F'])
-        # g_total = letter_counts['G'] * BASE_PRICES['G']
-        # h_total = self.calculate_h_total(letter_counts['H'])
-        # i_total = letter_counts['I'] * BASE_PRICES['I']
-        # j_total = letter_counts['J'] * BASE_PRICES['J']
-        # k_total = self.calculate_k_total(letter_counts['K'])
-        # l_total = letter_counts['L'] * BASE_PRICES['L']
-    
-        # # TODO order swap, free Ms for N
-        # n_total = self.calculate_n_total(letter_counts['N'])
-        # m_total = 0  # TODO
-        
-        # o_total = letter_counts['O'] * BASE_PRICES['O']
-        # p_total = self.calculate_p_total(letter_counts['P'])
-
-        # # TODO order swap, free Qs for R
-        # q_total = self.calculate_q_total(letter_counts['Q'])
-        # r_total = self.calculate_r_total(letter_counts['R'])
-
-        # s_total = letter_counts['S'] * BASE_PRICES['S']
-        # t_total = letter_counts['T'] * BASE_PRICES['T']
-        # u_total = self.calculate_u_total(letter_counts['U'])
-        # v_total = self.calculate_v_total(letter_counts['V'])
-        # w_total = letter_counts['W'] * BASE_PRICES['W']
-        # x_total = letter_counts['X'] * BASE_PRICES['X']
-        # y_total = letter_counts['Y'] * BASE_PRICES['Y']
-        # z_total = letter_counts['Z'] * BASE_PRICES['Z']
-
-        # # really dont want to also write all these out again, could just aggregate totals but still thinking of cleaner way
-        # # have to specifc funcs in meantime anyway
-        # return sum([a_total, b_total, c_total, d_total, e_total, f_total, g_total, h_total])
 
 

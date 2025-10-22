3731a8cd0ec58f5f0bff4822af5d32fe0c8a1aca
sbx_user1051 <sbx_user1051@169.254.39.17>
2025-10-22 12:52:32 +0000

    Wed Oct 22 12:52:32 UTC 2025

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,7 +1,30 @@
 
+"""
+Our price table and offers:
++------+-------+----------------+
+| Item | Price | Special offers |
++------+-------+----------------+
+| A    | 50    | 3A for 130     |
+| B    | 30    | 2B for 45      |
+| C    | 20    |                |
+| D    | 15    |                |
++------+-------+----------------+
+"""
+
+
 class CheckoutSolution:
 
     # skus = unicode string
     def checkout(self, skus):
-        return "test"
+        
+        a_count = skus.count('A')
+        b_count = skus.count('B')
+        c_count = skus.count('C')
+        d_count = skus.count('D')
+
+        # the site says AI allowed, I assume using copilot is allowed in that case, massive apologies if not
+
+        # I'm going to pause and read the page again.
+        a_total = (a_count // 3) * 130 + (a_count % 3) * 50
+
 

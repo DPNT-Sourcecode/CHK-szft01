f0965b0e555d88d4f6ee6aaeb44b5de9774b1f7e
sbx_user1051 <sbx_user1051@169.254.39.17>
2025-10-22 14:18:49 +0000

    Wed Oct 22 14:18:49 UTC 2025

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -152,38 +152,37 @@ class CheckoutSolution:
         b_total = self.calculate_b_total(letter_counts['B'], b_free_count)
 
         f_total = self.calculate_f_total(letter_counts['F'])
-
         g_total = letter_counts['G'] * BASE_PRICES['G']
         h_total = self.calculate_h_total(letter_counts['H'])
-        i_total = 
-        j_total = pass
+        i_total = letter_counts['I'] * BASE_PRICES['I']
+        j_total = letter_counts['J'] * BASE_PRICES['J']
         k_total = self.calculate_k_total(letter_counts['K'])
-        l_total = pass
+        l_total = letter_counts['L'] * BASE_PRICES['L']
     
         # TODO order swap, free Ms for N
         n_total = self.calculate_n_total(letter_counts['N'])
         m_total = pass
         
-        o_total = pass
+        o_total = letter_counts['O'] * BASE_PRICES['O']
         p_total = self.calculate_p_total(letter_counts['P'])
 
         # TODO order swap, free Qs for R
         q_total = self.calculate_q_total(letter_counts['Q'])
         r_total = self.calculate_r_total(letter_counts['R'])
 
-        s_total = pass
-        t_total = pass
+        s_total = letter_counts['S'] * BASE_PRICES['S']
+        t_total = letter_counts['T'] * BASE_PRICES['T']
         u_total = self.calculate_u_total(letter_counts['U'])
         v_total = self.calculate_v_total(letter_counts['V'])
-        w_total = pass
-        x_total = pass
-        y_total = pass
-        z_total = pass
+        w_total = letter_counts['W'] * BASE_PRICES['W']
+        x_total = letter_counts['X'] * BASE_PRICES['X']
+        y_total = letter_counts['Y'] * BASE_PRICES['Y']
+        z_total = letter_counts['Z'] * BASE_PRICES['Z']
 
-        
-        
-
+        # really dont want to also write all these out again, could just aggregate totals but still thinking of cleaner way
+        # have to specifc funcs in meantime anyway
         return sum([a_total, b_total, c_total, d_total, e_total, f_total])
+
 
 
 

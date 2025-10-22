1b2f8753bb46a935c8354447e553076a290724af
sbx_user1051 <sbx_user1051@169.254.39.17>
2025-10-22 13:51:49 +0000

    Wed Oct 22 13:51:49 UTC 2025

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -10,6 +10,26 @@ Our price table and offers:
 | D    | 15    |                        |
 | E    | 40    | 2E get one B free      |
 | F    | 10    | 2F get one F free      |
+| G    | 20    |                        |
+| H    | 10    | 5H for 45, 10H for 80  |
+| I    | 35    |                        |
+| J    | 60    |                        |
+| K    | 80    | 2K for 150             |
+| L    | 90    |                        |
+| M    | 15    |                        |
+| N    | 40    | 3N get one M free      |
+| O    | 10    |                        |
+| P    | 50    | 5P for 200             |
+| Q    | 30    | 3Q for 80              |
+| R    | 50    | 3R get one Q free      |
+| S    | 30    |                        |
+| T    | 20    |                        |
+| U    | 40    | 3U get one U free      |
+| V    | 50    | 2V for 90, 3V for 130  |
+| W    | 20    |                        |
+| X    | 90    |                        |
+| Y    | 10    |                        |
+| Z    | 50    |                        |
 +------+-------+------------------------+
 """
 import re
@@ -74,4 +94,5 @@ class CheckoutSolution:
         b_total = (b_count // 2) * 45 + (b_count % 2) * BASE_PRICES['B']
 
         return sum([a_total, b_total, c_total, d_total, e_total, f_count])
+
 
